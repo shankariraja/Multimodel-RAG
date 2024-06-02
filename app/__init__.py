3fcec63d4e1af525e08b68005f556aff803b1913
shankariraja <125146652+shankariraja@users.noreply.github.com>
2024-06-02 13:27:43 +0530

    fix init

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
-# app/__init__.py
-from flask import Flask
 from app.routes import routes
+from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+
+    from .routes import routes
     app.register_blueprint(routes)
+
     return app
+
+app = create_app()
